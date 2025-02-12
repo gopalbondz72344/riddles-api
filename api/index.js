c787130b568ea31d997be31f405e1b09771ea5c5
saigopal-dev <saigopal.mepstra@gmail.com>
2025-02-12 21:07:21 +0530

    cors error fix

--- api/index.js
+++ api/index.js
@@ -6,11 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 app.use(express.json());
 
 const RIDDLES_API = 'https://riddles-api.vercel.app/random';
