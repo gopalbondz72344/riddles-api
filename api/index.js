a073100321ad554e30a5aa93bbe5551b5141dd6d
saigopal-dev <saigopal.mepstra@gmail.com>
2025-02-12 23:22:34 +0530

    localhost origin with credentials deleted headers

--- api/index.js
+++ api/index.js
@@ -8,8 +8,6 @@ const port = 4000;
 
 app.use(cors({
   origin: "http://localhost:3000", // Allow frontend origin
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
   credentials: true
 }));
 app.options("*", cors());
